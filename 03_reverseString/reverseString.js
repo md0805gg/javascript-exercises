8cbafe1360ae7333dd0710324eeaa7c0150203cd
md0805gg-linux <112896171+md0805gg@users.noreply.github.com>
2024-02-12 19:47:42 +0100

    reverseString Loop solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let stringReversed = ''
+  for(i = -1; i >= (string.length) * (-1); i--) {
+    stringReversed += string.at(i);
+    }
+  return stringReversed;
 };
 
 // Do not edit below this line
