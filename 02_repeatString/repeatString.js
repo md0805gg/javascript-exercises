8ce7b183cf7082d8ca8f14019a19fba8fa252fef
md0805gg-linux <112896171+md0805gg@users.noreply.github.com>
2024-02-12 19:11:16 +0100

    Solve the exercise & unlock all tests
    Updated the code with the if statement, that takes
    negative numbers into consideration.
    Reorganized the code.
    Unlocked all 7 tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,12 +2,15 @@
 
 const repeatString = function(string, num) {
   let outcome = '';
-  for(i = 0; i < num; i++) {
-    outcome += string;
+  if (num < 0) {
+    outcome = 'ERROR'}
+    else {
+      for(i = 0; i < num; i++) {
+        outcome += string;
+      }
+    }
+    return outcome;
   }
-  return outcome;
-};
-
-
+  
 // Do not edit below this line
 module.exports = repeatString;
