16608ca77cd77e24e37c9f044c378d0fe5212eff
md0805gg-linux <112896171+md0805gg@users.noreply.github.com>
2024-02-12 19:16:21 +0100

    Update the code so it's closer to solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,16 +1,15 @@
 
 
 const repeatString = function(string, num) {
-  let outcome = '';
   if (num < 0) {
-    outcome = 'ERROR'}
-    else {
-      for(i = 0; i < num; i++) {
-        outcome += string;
-      }
-    }
-    return outcome;
+    return 'ERROR'
   }
-  
+  let outcome = '';
+  for(i = 0; i < num; i++) {
+    outcome += string;
+  }
+  return outcome;
+  }
+
 // Do not edit below this line
 module.exports = repeatString;
