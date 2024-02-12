b1bf412fae70e98960a496ee0a84230d8bdb8268
md0805gg-linux <112896171+md0805gg@users.noreply.github.com>
2024-02-12 09:24:48 +0100

    repeatString - write code to pass 1st test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+  let outcome = '';
+  for(i = 0; i < num; i++) {
+    outcome += string;
+  }
+  return outcome;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
