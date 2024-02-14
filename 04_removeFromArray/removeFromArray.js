6dd32974f87db0ee5ca7ba3d951654f37d3bc055
md0805gg-linux <112896171+md0805gg@users.noreply.github.com>
2024-02-14 21:07:19 +0100

    Write logic to create array with values to remove
    Still WIP, logic might change in future commits

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedValues) {
+  let array2 = [];
+   for (i = 0; i < removedValues.length; i++) {
+    if (array.includes(removedValues[i])) {
+      array2.push(removedValues[i]);
+   } 
+  }
+  
+ };
 
-};
+
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+
+//check item by item of array2 against array
+//if there is a match, return the item
+//remove the returned item from an array
+//if there is no match, continue
 
 // Do not edit below this line
 module.exports = removeFromArray;
