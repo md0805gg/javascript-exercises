60ee2c12624d440359458a86c42d6b8099e3613b
md0805gg-linux <112896171+md0805gg@users.noreply.github.com>
2024-02-15 19:54:07 +0100

    switched to solution method for analysis

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,30 +1,11 @@
-const removeFromArray = function(array, ...removedValues) {
-  let array2 = [];
-   for (i = 0; i < removedValues.length; i++) {
-    if (array.includes(removedValues[i])) {
-      array2.push(removedValues[i]);
-   }
-   }
-
-   let array3 = [];
-   for(i = 0; i < array.length; i++) {
-    if(array2.includes(array[i])){
-      continue;
-    } else {
-      array3.push(array[i]);
+const removeFromArray = function (array, ...args) {
+  const newArray = [];
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
     }
-    }
-    return array3;
-  };
+  });
+  return newArray;
+};
   
-
-
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
-
-//check item by item of array2 against array
-//if there is a match, return the item
-//remove the returned item from an array
-//if there is no match, continue
-
-// Do not edit below this line
 module.exports = removeFromArray;
