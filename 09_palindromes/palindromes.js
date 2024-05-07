11835586c73d2054aa739a670f413ce89b83a5a8
md0805gg-linux <112896171+md0805gg@users.noreply.github.com>
2024-05-07 16:41:39 +0200

    Start palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  // create var cottaining array of forbidden characters
+  let forbidden = [',','.',' '];
+//convert input into array of characters
+  inputToArray = input.split('');
+//iterate though each character with includes forbidden characters
 };
-
 // Do not edit below this line
 module.exports = palindromes;
