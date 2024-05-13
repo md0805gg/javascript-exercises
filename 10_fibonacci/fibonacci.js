541e816c919eeb3e13fa7efee1f858352113a044
md0805gg-linux <112896171+md0805gg@users.noreply.github.com>
2024-05-13 17:12:33 +0200

    Create loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  arrayForSequence = [];
+  for (let i = 0; i <= number; i++) {
+    arrayForSequence.push(i);
+  }
+  numbersForSequence = arrayForSequence.slice(-3,-1);
+  finalNumber = numbersForSequence.reduce((sum, current) => sum + current, 0);
+  return finalNumber;
 };
 
 // Do not edit below this line
