e1a8a702e413183bb577915c284e26660f4cb4b2
md0805gg-linux <112896171+md0805gg@users.noreply.github.com>
2024-05-17 08:40:04 +0200

    Find person with greatest age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  let objectWithAge = array.map((item) => 
+     ({
+      name: item.name,
+      age: item.yearOfDeath - item.yearOfBirth
+    })
+  );
+  return greatestAge = objectWithAge.reduce((previous, current) =>  
+  previous.age > current.age ? previous : current
+  );
 };
+ 
 
 // Do not edit below this line
 module.exports = findTheOldest;
